6bc10f3ace77e87d2eac6e80809726d4be8a3e3f
Ray <18511234520@163.com>
2016-05-18 22:02:32 +0800

    sign
    code sign

--- ZTDraggableViewDemo/ZTDraggableView.h
+++ ZTDraggableViewDemo/ZTDraggableView.h
@@ -7,7 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-
+///the angle controls the animation angle after user click the button
 #define ROTATION_ANGLE M_PI/8
 #define CLICK_ANIMATION_TIME 0.5
 #define RESET_ANIMATION_TIME 0.3
@@ -15,7 +15,9 @@
 @protocol ZTDraggableViewDelegate <NSObject>
 -(void)cardSwiped:(UIView *)card LorR:(BOOL)isRight;
 -(void)moveCards:(CGFloat)distance;
+///if transform the card not enough, move the card back to origin position
 -(void)moveBackCards;
+///if the above card been removed,
 -(void)adjustOtherCards;
 @end
 
@@ -23,21 +25,23 @@
 
 @property (weak) id <ZTDraggableViewDelegate> delegate;
 
-@property (nonatomic, strong)UIPanGestureRecognizer *panGestureRecognizer;
-@property (nonatomic)CGPoint originalPoint;
-@property (nonatomic)CGPoint originalCenter;
-@property (nonatomic)CGAffineTransform originalTransform;
-@property (nonatomic)BOOL canPan;
 
+///set the cards can move, Default is NO;
+@property (nonatomic)BOOL canPan;
+///Basic Cards' information
 @property (nonatomic)NSDictionary *userInfo;
 
 @property (nonatomic,strong)UIImageView *headerImageView;
 @property (nonatomic,strong)UILabel *numLabel;
 @property (nonatomic,strong)UIButton* yesButton;
 @property (nonatomic,strong)UIButton* noButton;
+@property (nonatomic, strong)UIPanGestureRecognizer *panGestureRecognizer;
+@property (nonatomic)CGPoint originalPoint;
+@property (nonatomic)CGPoint originalCenter;
+@property (nonatomic)CGAffineTransform originalTransform;
 
--(void)leftClickAction;
--(void)rightClickAction;
+//-(void)leftClickAction;
+//-(void)rightClickAction;
 
 @end
 
--- ZTDraggableViewDemo/ZTDraggableView.m
+++ ZTDraggableViewDemo/ZTDraggableView.m
@@ -17,19 +17,20 @@
 #define SCALE_MAX .93
 #define ROTATION_MAX 1
 #define ROTATION_STRENGTH lengthFit(414)
+#define icon_LikeButton @"icon_smilyFace"
+#define icon_UnLikeButton @"icon_cry"
 
 #define BUTTON_WIDTH lengthFit(40)
 
-@implementation ZTDraggableView{
+@implementation ZTDraggableView
+{
     CGFloat xFromCenter;
     CGFloat yFromCenter;
 }
 
-- (id)initWithFrame:(CGRect)frame
-{
+- (id)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
-        
         self.backgroundColor = [UIColor whiteColor];
         self.layer.cornerRadius = 4;
         self.layer.shadowRadius = 3;
@@ -40,63 +41,55 @@
         self.panGestureRecognizer = [[UIPanGestureRecognizer alloc]initWithTarget:self action:@selector(beingDragged:)];
         [self addGestureRecognizer:self.panGestureRecognizer];
         
-        UIView *bgView=[[UIView alloc]initWithFrame:CGRectMake(0, 0, self.frame.size.width, self.frame.size.height)];
-        bgView.layer.cornerRadius=4;
-        bgView.clipsToBounds=YES;
-        [self addSubview:bgView];
-        
-        self.noButton = [[UIButton alloc]initWithFrame:CGRectMake(lengthFit(47), self.frame.size.height-lengthFit(13)-BUTTON_WIDTH, BUTTON_WIDTH, BUTTON_WIDTH)];
-        [self.noButton setBackgroundImage:[UIImage imageNamed:@"哭脸"] forState:UIControlStateNormal];
-        [self.noButton addTarget:self action:@selector(leftClickAction) forControlEvents:UIControlEventTouchUpInside];
-        self.yesButton = [[UIButton alloc]initWithFrame:CGRectMake(self.frame.size.width-lengthFit(47)-BUTTON_WIDTH, self.frame.size.height-lengthFit(13)-BUTTON_WIDTH, BUTTON_WIDTH, BUTTON_WIDTH)];
-        [self.yesButton setBackgroundImage:[UIImage imageNamed:@"笑脸"] forState:UIControlStateNormal];
-        [self.yesButton addTarget:self action:@selector(rightClickAction) forControlEvents:UIControlEventTouchUpInside];
-        [bgView addSubview:self.noButton];
-        [bgView addSubview:self.yesButton];
-        
-        self.headerImageView=[[UIImageView alloc]initWithFrame:CGRectMake(0, 0, self.frame.size.width,self.frame.size.width)];
-        self.headerImageView.backgroundColor=[UIColor colorWithRed:220/255.0 green:220/255.0 blue:220/255.0 alpha:1];
-        self.headerImageView.userInteractionEnabled=YES;
-        [bgView addSubview:self.headerImageView];
-        UITapGestureRecognizer *tap=[[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(tap:)];
-        [self.headerImageView addGestureRecognizer:tap];
-        
-        
-        self.numLabel=[[UILabel alloc]initWithFrame:CGRectMake(0, self.headerImageView.frame.size.height-lengthFit(20)-4, self.frame.size.width, lengthFit(20))];
-        self.numLabel.font=[UIFont systemFontOfSize:15];
-        self.numLabel.textAlignment=NSTextAlignmentCenter;
-        [bgView addSubview:self.numLabel];
-        
-        self.layer.allowsEdgeAntialiasing=YES;
-        bgView.layer.allowsEdgeAntialiasing=YES;
-        self.headerImageView.layer.allowsEdgeAntialiasing=YES;
-        
+        UIView * backgroundView = [self backgroundView];
+        [self addSubview:backgroundView];
     }
     return self;
 }
 
--(void)tap:(UITapGestureRecognizer*)sender{
+- (UIView *)backgroundView {
+    UIView * backgroundView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, self.frame.size.width, self.frame.size.height)];
+    backgroundView.layer.cornerRadius = 4;
+    backgroundView.clipsToBounds = YES;
     
-    if (self.canPan==NO) {
-        return;
-    }
-
-    NSLog(@"tap");
+    ///set buttons' position
+    CGFloat buttonOriginY = self.frame.size.height - lengthFit(13) - BUTTON_WIDTH;
+    CGFloat inserts = ceilf(lengthFit(47));
+    self.noButton.frame = CGRectMake(inserts, buttonOriginY, BUTTON_WIDTH, BUTTON_WIDTH);
+    self.yesButton.frame = CGRectMake(self.frame.size.width - inserts - BUTTON_WIDTH, buttonOriginY, BUTTON_WIDTH, BUTTON_WIDTH);
     
+    [backgroundView addSubview:self.noButton];
+    [backgroundView addSubview:self.yesButton];
+    [backgroundView addSubview:self.headerImageView];
+    [backgroundView addSubview:self.numLabel];
+    
+    /**
+     Set /allowsEdgeAntialiasing/ value yes --
+     this layer is allowed to antialias its edges
+     */
+    self.layer.allowsEdgeAntialiasing = YES;
+    backgroundView.layer.allowsEdgeAntialiasing = YES;
+    self.headerImageView.layer.allowsEdgeAntialiasing = YES;
+    return backgroundView;
 }
 
--(void)layoutSubviews{
-    
+///function when user touch the image
+-(void)tap:(UITapGestureRecognizer*)gesture {
+    if (self.canPan == NO) {
+        return;
+    }
+    NSLog(@"imageView had been touched");
+}
+
+-(void)layoutSubviews {
     self.numLabel.text = self.userInfo[@"num"];
 }
 
-#pragma mark - 拖动
--(void)beingDragged:(UIPanGestureRecognizer *)gestureRecognizer
-{
-    if (self.canPan==NO) {
+#pragma mark - Dragging
+-(void)beingDragged:(UIPanGestureRecognizer *)gestureRecognizer {
+    if (self.canPan == NO) {
         return;
     }
-    
     xFromCenter = [gestureRecognizer translationInView:self].x;
     yFromCenter = [gestureRecognizer translationInView:self].y;
     
@@ -105,28 +98,14 @@
         case UIGestureRecognizerStateBegan:{
             
         };
-            
+            ///transform while user dragging view
         case UIGestureRecognizerStateChanged:{
-            
-            CGFloat rotationStrength = MIN(xFromCenter / ROTATION_STRENGTH, ROTATION_MAX);
-            
-            CGFloat rotationAngel = (CGFloat) (ROTATION_ANGLE * rotationStrength);
-            
-            CGFloat scale = MAX(1 - fabs(rotationStrength) / SCALE_STRENGTH, SCALE_MAX);
-            
-            self.center = CGPointMake(self.originalCenter.x + xFromCenter, self.originalCenter.y + yFromCenter);
-            
-            CGAffineTransform transform = CGAffineTransformMakeRotation(rotationAngel);
-            
-            CGAffineTransform scaleTransform = CGAffineTransformScale(transform, scale, scale);
-            
-            self.transform = scaleTransform;
+            self.transform = [self transformWhileDragging];
             [self updateOverlay:xFromCenter];
-            
             break;
         };
         case UIGestureRecognizerStateEnded: {
- 
+            
             [self followUpActionWithDistance:xFromCenter andVelocity:[gestureRecognizer velocityInView:self.superview]];
             break;
         };
@@ -135,151 +114,172 @@
         case UIGestureRecognizerStateFailed:break;
     }
 }
+///the position while user dragging the view
+- (CGAffineTransform)transformWhileDragging {
+    CGFloat rotationStrength = MIN(xFromCenter / ROTATION_STRENGTH, ROTATION_MAX);
+    
+    CGFloat rotationAngel = (CGFloat) (ROTATION_ANGLE * rotationStrength);
+    
+    CGFloat scale = MAX(1 - fabs(rotationStrength) / SCALE_STRENGTH, SCALE_MAX);
+    
+    self.center = CGPointMake(self.originalCenter.x + xFromCenter, self.originalCenter.y + yFromCenter);
+    
+    CGAffineTransform transform = CGAffineTransformMakeRotation(rotationAngel);
+    
+    return CGAffineTransformScale(transform, scale, scale);
+}
 
 #pragma mark - 滑动中事件
+///change yes/no button's size when user dragging view to right/left
 -(void)updateOverlay:(CGFloat)distance
 {
+    CGFloat scale = 1 + 0.5 * fabs(distance/PAN_DISTANCE);
     if (distance > 0) {
-        self.yesButton.transform=CGAffineTransformMakeScale(1+0.5*fabs(distance/PAN_DISTANCE), 1+0.5*fabs(distance/PAN_DISTANCE));
+        self.yesButton.transform = CGAffineTransformMakeScale(scale, scale);
     } else {
-        self.noButton.transform=CGAffineTransformMakeScale(1+0.5*fabs(distance/PAN_DISTANCE), 1+0.5*fabs(distance/PAN_DISTANCE));
+        self.noButton.transform = CGAffineTransformMakeScale(scale, scale);
     }
-
-    [self.delegate moveCards:distance];
+    
+    if (self.delegate && [self.delegate respondsToSelector:@selector(moveCards:)]) {
+        [self.delegate moveCards:distance];
+    }
 }
 
 #pragma mark - 后续动作判断
+///drag the view over it's container
 - (void)followUpActionWithDistance:(CGFloat)distance andVelocity:(CGPoint)velocity
 {
-    if (xFromCenter > 0 && (distance > ACTION_MARGIN_RIGHT||velocity.x > ACTION_VELOCITY)) {
-        [self rightAction:velocity];
-    } else if (xFromCenter <0 && (distance < -ACTION_MARGIN_LEFT||velocity.x < -ACTION_VELOCITY)) {
-        [self leftAction:velocity];
+    if (xFromCenter > 0 && (distance > ACTION_MARGIN_RIGHT || velocity.x > ACTION_VELOCITY)) {
+        [self setChangesToLeftOrRight:YES andVelocity:velocity];
+    } else if (xFromCenter < 0 && (distance < - ACTION_MARGIN_LEFT || velocity.x < - ACTION_VELOCITY)) {
+        [self setChangesToLeftOrRight:NO andVelocity:velocity];
     } else {
+        __weak typeof(self) weakSelf = self;
         [UIView animateWithDuration:RESET_ANIMATION_TIME
                          animations:^{
-                             self.center = self.originalCenter;
-                             self.transform = CGAffineTransformMakeRotation(0);
-                             self.yesButton.transform=CGAffineTransformMakeScale(1, 1);
-                             self.noButton.transform=CGAffineTransformMakeScale(1, 1);
-
+                             weakSelf.center = weakSelf.originalCenter;
+                             weakSelf.transform = CGAffineTransformMakeRotation(0);
+                             weakSelf.yesButton.transform = CGAffineTransformMakeScale(1, 1);
+                             weakSelf.noButton.transform = CGAffineTransformMakeScale(1, 1);
                          }];
-        [self.delegate moveBackCards];
+        if (self.delegate && [self.delegate respondsToSelector:@selector(moveCards:)]) {
+            [self.delegate moveBackCards];
+        }
     }
 }
-
-#pragma mark - 右滑后续事件
--(void)rightAction:(CGPoint)velocity
-{
-    CGFloat distanceX=[[UIScreen mainScreen]bounds].size.width+CARD_WIDTH+self.originalCenter.x;//横向移动距离
-    CGFloat distanceY=distanceX*yFromCenter/xFromCenter;//纵向移动距离
-    CGPoint finishPoint = CGPointMake(self.originalCenter.x+distanceX, self.originalCenter.y+distanceY);//目标center点
-    
-    CGFloat vel=sqrtf(pow(velocity.x, 2)+pow(velocity.y, 2));//滑动手势横纵合速度
-    CGFloat displace=sqrt(pow(distanceX-xFromCenter,2)+pow(distanceY-yFromCenter,2));//需要动画完成的剩下距离
-    
-    CGFloat duration=fabs(displace/vel);//动画时间
-    
-    if (duration>0.6) {
-        duration=0.6;
-    }else if(duration<0.3){
-        duration=0.3;
+///after user drag the view to the left or right, change its status
+- (void)setChangesToLeftOrRight:(BOOL)isRight andVelocity:(CGPoint)velocity {
+    //横向移动距离
+    CGFloat distanceX;
+    if (isRight) {
+        distanceX = [[UIScreen mainScreen]bounds].size.width + CARD_WIDTH + self.originalCenter.x;
+    } else {
+        distanceX = - CARD_WIDTH - self.originalCenter.x;
     }
+    CGFloat distanceY = distanceX * yFromCenter / xFromCenter;//纵向移动距离
+    CGPoint finishPoint = CGPointMake(self.originalCenter.x + distanceX, self.originalCenter.y + distanceY);//目标center点
     
+    CGFloat vel = sqrtf(pow(velocity.x, 2) + pow(velocity.y, 2));//滑动手势横纵合速度
+    CGFloat displace = sqrt(pow(distanceX - xFromCenter, 2) + pow(distanceY - yFromCenter, 2));//需要动画完成的剩下距离
+    
+    CGFloat duration = fabs(displace / vel);//动画时间
+    
+    if (duration > 0.6) {
+        duration = 0.6;
+    } else if (duration < 0.3) {
+        duration = 0.3;
+    }
+    [self startTheAnimate:duration andDirection:isRight destination:finishPoint];
+}
+- (void)startTheAnimate:(CGFloat)duration andDirection:(BOOL)isRight destination:(CGPoint)destination {
+    __weak typeof(self) weakSelf = self;
     [UIView animateWithDuration:duration
                      animations:^{
-
-                         self.yesButton.transform=CGAffineTransformMakeScale(1.5, 1.5);
-                         self.center = finishPoint;
-                         self.transform = CGAffineTransformMakeRotation(ROTATION_ANGLE);
+                         UIButton * button = isRight ? weakSelf.yesButton : weakSelf.noButton;
+                         button.transform = CGAffineTransformMakeScale(1.5, 1.5);
+                         weakSelf.center = destination;
+                         weakSelf.transform = CGAffineTransformMakeRotation(isRight ? ROTATION_ANGLE : - ROTATION_ANGLE);
                      }completion:^(BOOL complete){
-
-                         self.yesButton.transform=CGAffineTransformMakeScale(1, 1);
-                         [self.delegate cardSwiped:self LorR:YES];
+                         UIButton * button = isRight ? self.yesButton : self.noButton;
+                         button.transform = CGAffineTransformMakeScale(1, 1);
+                         if (self.delegate && [self.delegate respondsToSelector:@selector(cardSwiped:LorR:)]) {
+                             [self.delegate cardSwiped:self LorR:isRight];
+                         }
                      }];
-    [self.delegate adjustOtherCards];
-    
-}
-
-#pragma mark - 左滑后续事件
--(void)leftAction:(CGPoint)velocity
-{
-    CGFloat distanceX=-CARD_WIDTH-self.originalCenter.x;//横向移动距离
-    CGFloat distanceY=distanceX*yFromCenter/xFromCenter;//纵向移动距离
-    CGPoint finishPoint = CGPointMake(self.originalCenter.x+distanceX, self.originalCenter.y+distanceY);//目标center点
-    
-    CGFloat vel=sqrtf(pow(velocity.x, 2)+pow(velocity.y, 2));//滑动手势横纵合速度
-    CGFloat displace=sqrt(pow(distanceX-xFromCenter,2)+pow(distanceY-yFromCenter,2));//需要动画完成的剩下距离
-    
-    CGFloat duration=fabs(displace/vel);//动画时间
-    
-    if (duration>0.6) {
-        duration=0.6;
-    }else if(duration<0.3){
-        duration=0.3;
+    if (self.delegate && [self.delegate respondsToSelector:@selector(adjustOtherCards)]) {
+        
+        [self.delegate adjustOtherCards];
     }
-    
-    [UIView animateWithDuration:duration
-                     animations:^{
-  
-                         self.noButton.transform=CGAffineTransformMakeScale(1.5, 1.5);
-                         self.center = finishPoint;
-                         self.transform = CGAffineTransformMakeRotation(-ROTATION_ANGLE);
-                     }completion:^(BOOL complete){
-
-                         self.noButton.transform=CGAffineTransformMakeScale(1, 1);
-                         [self.delegate cardSwiped:self LorR:NO];
-                     }];
-    [self.delegate adjustOtherCards];
-    
 }
-
 #pragma mark - 点击右滑事件
--(void)rightClickAction
+-(void)rightClickAction:(UIButton *)sender
 {
-    if (self.canPan==NO) {
-        return;
-    }
-
-    CGPoint finishPoint = CGPointMake([[UIScreen mainScreen]bounds].size.width+CARD_WIDTH*2/3, 2*PAN_DISTANCE+self.frame.origin.y);
-    [UIView animateWithDuration:CLICK_ANIMATION_TIME
-                     animations:^{
-  
-                         self.yesButton.transform=CGAffineTransformMakeScale(1.5, 1.5);
-                         self.center = finishPoint;
-                         self.transform = CGAffineTransformMakeRotation(ROTATION_ANGLE);
-                     }completion:^(BOOL complete){
-
-                         self.yesButton.transform=CGAffineTransformMakeScale(1, 1);
-                         [self.delegate cardSwiped:self LorR:YES];
-                     }];
-    
-    [self.delegate adjustOtherCards];
+    [self userClickButtons:YES];
     
 }
-
 #pragma mark - 点击左滑事件
--(void)leftClickAction
+-(void)leftClickAction:(UIButton *)sender
 {
-    if (self.canPan==NO) {
+    [self userClickButtons:NO];
+}
+
+- (void)userClickButtons:(BOOL)isSmilyButton {
+    if (self.canPan == NO) {
         return;
     }
+    CGPoint finishPoint;
+    if (isSmilyButton) {
+        finishPoint = CGPointMake([[UIScreen mainScreen]bounds].size.width + CARD_WIDTH * 2 / 3, 2 * PAN_DISTANCE + self.frame.origin.y);
+    } else {
+        finishPoint = CGPointMake(- CARD_WIDTH * 2 / 3, 2 * PAN_DISTANCE + self.frame.origin.y);
+    }
+    [self startTheAnimate:CLICK_ANIMATION_TIME andDirection:isSmilyButton destination:finishPoint];
+}
 
-    CGPoint finishPoint = CGPointMake(-CARD_WIDTH*2/3, 2*PAN_DISTANCE+self.frame.origin.y);
-    [UIView animateWithDuration:CLICK_ANIMATION_TIME
-                     animations:^{
- 
-                         self.noButton.transform=CGAffineTransformMakeScale(1.5, 1.5);
-                         self.center = finishPoint;
-                         self.transform = CGAffineTransformMakeRotation(-ROTATION_ANGLE);
-                     }completion:^(BOOL complete){
-
-                         self.noButton.transform=CGAffineTransformMakeScale(1, 1);
-                         [self.delegate cardSwiped:self LorR:NO];
-                     }];
-    
-    [self.delegate adjustOtherCards];
-    
+///numLabel shows current index of headerImageView
+- (UILabel *)numLabel {
+    if (!_numLabel) {
+        CGFloat numLabelHeight = ceilf(lengthFit(20));
+        CGFloat numLabelOriginY = self.headerImageView.frame.size.height - numLabelHeight - 4;
+        _numLabel = [[UILabel alloc]initWithFrame:CGRectMake(0, numLabelOriginY, self.frame.size.width, numLabelHeight)];
+        _numLabel.font = [UIFont systemFontOfSize:15];
+        _numLabel.textAlignment = NSTextAlignmentCenter;
+        
+    }
+    return _numLabel;
+}
+///set noButton
+- (UIButton *)noButton {
+    if (!_noButton) {
+        _noButton = [UIButton buttonWithType:UIButtonTypeSystem];
+        [_noButton setBackgroundImage:[UIImage imageNamed:icon_UnLikeButton]
+                             forState:UIControlStateNormal];
+        [_noButton addTarget:self
+                      action:@selector(leftClickAction:)
+            forControlEvents:UIControlEventTouchUpInside];
+    }
+    return _noButton;
+}
+///set yeButton
+- (UIButton *)yesButton {
+    if (!_yesButton) {
+        _yesButton = [UIButton buttonWithType:UIButtonTypeSystem];
+        [_yesButton setBackgroundImage:[UIImage imageNamed:icon_LikeButton]
+                              forState:UIControlStateNormal];
+        [_yesButton addTarget:self action:@selector(rightClickAction:)
+             forControlEvents:UIControlEventTouchUpInside];
+    }
+    return _yesButton;
+}
+- (UIImageView *)headerImageView {
+    if (!_headerImageView) {
+        _headerImageView = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, self.frame.size.width, self.frame.size.width)];
+        _headerImageView.backgroundColor = [UIColor colorWithRed:220/255.0 green:220/255.0 blue:220/255.0 alpha:1];
+        _headerImageView.userInteractionEnabled = YES;
+        UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(tap:)];
+        [_headerImageView addGestureRecognizer:tap];
+    }
+    return _headerImageView;
 }
 
 @end
